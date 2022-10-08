6551d411ff8cd84ccd2a7749c6f143530d359098
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-10-08 09:51:11 +0300

    Updated xml-comments for thread pool interface.

--- yakov.Lab1/yakov.ThreadPool/IThreadPool.cs
+++ yakov.Lab1/yakov.ThreadPool/IThreadPool.cs
@@ -8,11 +8,25 @@ namespace yakov.ThreadPool
 {
     public interface IDynamicThreadPool
     {
+        /// <summary>
+        /// Actions to invoke on task end.
+        /// </summary>
         public event Action OnTaskComplete;
 
+        /// <summary>
+        /// Maximum amount of threads.
+        /// </summary>
         public uint MaxThreadsCount { get; set; }
 
+        /// <summary>
+        /// Add new task to queue.
+        /// </summary>
+        /// <param name="newTask">Task to add.</param>
         public void EnqueueTask(Action newTask);
+        /// <summary>
+        /// Show if all tasks complete.
+        /// </summary>
+        /// <returns>True, if all tasks, that were added - complete. Other way - false.</returns>
         public bool IsAllTasksComplete();
     }
 }
