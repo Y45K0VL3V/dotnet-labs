fca6f7c41085f18a5c953365c065d9e8335c311b
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-10-20 20:49:44 +0300

    Implemented OSHandle to close handle by descriptor.

---
+++ yakov.SPPLabs/yakov.Lab2/OSHandle.cs
@@ -0,0 +1,60 @@
+﻿using System.Runtime.InteropServices;
+
+namespace yakov.Lab2
+{
+    /// <summary>
+    /// Provides control over extern handles.
+    /// </summary>
+    public class OSHandle : IDisposable
+    {
+        /// <summary>
+        /// Current handle.
+        /// </summary>
+        public IntPtr Handle { get; set; }
+
+        /// <summary>
+        /// Close the handle.
+        /// </summary>
+        /// <param name="handle">Handle to close</param>
+        /// <returns>True, if handle closed</returns>
+        [DllImport("Kernel32")]
+        private extern static Boolean CloseHandle(IntPtr handle);
+        
+        /// <summary>
+        /// Auto-closing handle.
+        /// </summary>
+        ~OSHandle()
+        {
+            Dispose(disposing: false);
+        }
+
+        #region IDisposable
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposed)
+            {
+                if (disposing)
+                {
+                    // Close managed resources.
+                }
+
+                CloseHandle(Handle);
+                Handle = IntPtr.Zero;
+
+                disposed = true;
+            }
+        }
+
+        /// <summary>
+        /// Manual close handle.
+        /// </summary>
+        public void Dispose()
+        {
+            Dispose(disposing: true);
+            GC.SuppressFinalize(this);
+        }
+        #endregion
+    }
+}
