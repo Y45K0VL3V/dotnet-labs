f409e7d60eee1bdfd8a302df2e7839dbf404234b
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-10-21 02:10:13 +0300

    Created LogBuffer structure.

---
+++ yakov.SPPLabs/yakov.Logger/LogBuffer.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace yakov.Logger
+{
+    public sealed class LogBuffer
+    {
+        public LogBuffer(string logFilePath, uint timeout, uint bufMaxElements)
+        {
+            LogFilePath = logFilePath;
+            Timeout = timeout;
+            BufferMaxElements = bufMaxElements;
+        }
+
+        private List<string> _items = new();
+
+        public string LogFilePath { get; private set; }
+        public uint Timeout { get; private set; }
+        public uint BufferMaxElements { get; private set; }
+
+        private void WriteToFile()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(string item)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
