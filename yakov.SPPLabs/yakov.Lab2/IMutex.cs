792886a9d6afbe55d132fa19917c8fc6f1b3663c
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-10-20 18:36:36 +0300

    Implemented mutex with interlocked operations. Added xml-comments.

---
+++ yakov.SPPLabs/yakov.Lab2/IMutex.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace yakov.Lab2
+{
+    public interface IMutex
+    {
+        /// <summary>
+        /// Lock mutex.
+        /// </summary>
+        public void Lock();
+
+        /// <summary>
+        /// Unlock mutex.
+        /// </summary>
+        public void Unlock();
+    }
+}
---
+++ yakov.SPPLabs/yakov.Lab2/Mutex.cs
@@ -0,0 +1,35 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace yakov.Lab2
+{
+    /// <summary>
+    /// Realize mutex for thread locks.
+    /// </summary>
+    public class Mutex : IMutex
+    {
+        /// <summary>
+        /// Contains thread's ID, that locked mutex. 
+        /// ID = -1 means mutex not locked.
+        /// </summary>
+        private int _activeThreadId = -1;
+        
+        /// <summary>
+        /// Lock mutex.
+        /// </summary>
+        public void Lock()
+        {
+            var currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            while (Interlocked.CompareExchange(ref _activeThreadId, currentThreadId, -1) != -1)
+                Thread.Sleep(10);
+        }
+
+        /// <summary>
+        /// Unlock mutex.
+        /// </summary>
+        public void Unlock()
+        {
+            var currentThreadId = Thread.CurrentThread.ManagedThreadId;
+            Interlocked.CompareExchange(ref _activeThreadId, -1, currentThreadId);
+        }
+    }
+}
