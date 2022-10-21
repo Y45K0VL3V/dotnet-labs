55bbf5561d664b1d102d2b67911eb62bacd3a527
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-10-21 03:17:13 +0300

    Add custom attribute and program, that allows to get public types with this attribute.

---
+++ yakov.SPPLabs/yakov.Lab4/ExportClassAttribute.cs
@@ -0,0 +1,9 @@
+﻿
+namespace yakov.Lab4
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class ExportClassAttribute : Attribute
+    {
+        // Imagine some meta-info.
+    }
+}
---
+++ yakov.SPPLabs/yakov.Lab4/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using yakov.AssemblyControl;
+using yakov.Lab4;
+
+Console.WriteLine("Enter assembly path:");
+string path = Console.ReadLine();
+
+AssemblyLoader assemblyLoader = new();
+Assembly? loadedAssembly;
+if ((loadedAssembly = assemblyLoader.LoadNew(path)) != null)
+{
+    var types = loadedAssembly.GetTypes((t) => t.IsPublic && t.GetCustomAttribute(typeof(ExportClassAttribute)) != null);
+
+    if (types != null)
+        foreach (var type in types)
+            Console.WriteLine(type.FullName);
+}
