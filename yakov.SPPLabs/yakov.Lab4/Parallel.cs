6018b816328e9232b384ff18822e92f89b2b0c35
Y45K0VL3V <yakovlevvadim666@gmail.com>
2022-10-23 18:19:56 +0300

    Implemented Parallel.WaitAll, that use prev. created TaskQueue.

---
+++ yakov.SPPLabs/yakov.Lab4/Parallel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using yakov.ThreadPool;
+
+namespace yakov.Lab4
+{
+    public static class Parallel
+    {
+        public static void WaitAll(Action[] actions)
+        {
+            TaskQueue taskQueue = new((uint)actions.Length);
+            foreach (var action in actions)
+                taskQueue.EnqueueTask(action);
+
+            while (!taskQueue.IsAllTasksComplete()) { }
+        }
+    }
+}
--- yakov.SPPLabs/yakov.Lab4/Program.cs
+++ yakov.SPPLabs/yakov.Lab4/Program.cs
@@ -1,17 +1,21 @@
 ﻿using System.Reflection;
 using yakov.AssemblyControl;
 using yakov.Lab4;
+using yakov.ThreadPool;
+using Parallel = yakov.Lab4.Parallel;
 
-Console.WriteLine("Enter assembly path:");
-string path = Console.ReadLine();
+//Console.WriteLine("Enter assembly path:");
+//string path = Console.ReadLine();
 
-AssemblyLoader assemblyLoader = new();
-Assembly? loadedAssembly;
-if ((loadedAssembly = assemblyLoader.LoadNew(path)) != null)
-{
-    var types = loadedAssembly.GetTypes((t) => t.IsPublic && t.GetCustomAttribute(typeof(ExportClassAttribute)) != null);
+//AssemblyLoader assemblyLoader = new();
+//Assembly? loadedAssembly;
+//if ((loadedAssembly = assemblyLoader.LoadNew(path)) != null)
+//{
+//    var types = loadedAssembly.GetTypes((t) => t.IsPublic && t.GetCustomAttribute(typeof(ExportClassAttribute)) != null);
 
-    if (types != null)
-        foreach (var type in types)
-            Console.WriteLine(type.FullName);
-}
+//    if (types != null)
+//        foreach (var type in types)
+//            Console.WriteLine(type.FullName);
+//}
+
+Parallel.WaitAll(new Action[] { () => Console.WriteLine("f"), () => Console.WriteLine("sdfsdf")});
